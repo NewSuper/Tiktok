1ef873c8a58baf3710e732ea695844cd2d02a651
NewSuper <newsuper6892@sina.com>
2022-01-04 08:40:35 +0800

    发现页组件

---
+++ module_discover/src/main/java/com/aitd/module_discover/model/BannerBean.kt
@@ -0,0 +1,3 @@
+package com.aitd.module_discover.model
+
+class BannerBean (val imgUrl:String,val imgTitle:String,val bannerId:String,val langCode:String,val sort:String,val type:Int)
---
+++ module_discover/src/main/java/com/aitd/module_discover/model/RecommendBean.kt
@@ -0,0 +1,3 @@
+package com.aitd.module_discover.model
+
+class RecommendBean (val coverUrl:String,val isTopping:String,val publishTime:String,val author:String,val remark:String,)
