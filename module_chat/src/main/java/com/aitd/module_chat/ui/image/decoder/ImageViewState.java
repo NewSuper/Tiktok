6eab6f161d55bb0584ba8ec6f6df963c47018dab
NewSuper <newsuper6892@sina.com>
2022-01-07 17:00:22 +0800

    选择图片

---
+++ module_chat/src/main/java/com/aitd/module_chat/ui/image/decoder/ImageViewState.java
@@ -0,0 +1,35 @@
+package com.aitd.module_chat.ui.image.decoder;
+
+import android.graphics.PointF;
+
+import androidx.annotation.NonNull;
+
+public class ImageViewState {
+
+    private final float scale;
+
+    private final float centerX;
+
+    private final float centerY;
+
+    private final int orientation;
+
+    public ImageViewState(float scale, @NonNull PointF center, int orientation) {
+        this.scale = scale;
+        this.centerX = center.x;
+        this.centerY = center.y;
+        this.orientation = orientation;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    @NonNull public PointF getCenter() {
+        return new PointF(centerX, centerY);
+    }
+
+    public int getOrientation() {
+        return orientation;
+    }
+}
